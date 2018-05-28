31019db758184f0f483fb381a69da2807177c31a
HXCloud <39583019+HXCloud@users.noreply.github.com>
2018-05-28 17:45:51 +0800

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -1,5 +1,33 @@
-/*搜索栏*/
 $(function(){
+    $(".button_icon a").each(function(){
+        if($($(this))[0].href==String(window.location))
+            $(this)
+                .addClass("is-actived")
+                .siblings()
+                .removeClass("is-actived");
+    });
+
+    $(".menu-button a").each(function(){
+        if($($(this))[0].href==String(window.location))
+            $(this)
+            .addClass("is-actived")
+            .siblings()
+            .removeClass("is-actived");
+    });
+
+    var bswiper = new Swiper('.banner-swiper', {
+        pagination: {
+            el: '.banner-pagination',
+            dynamicBullets: true,
+        },
+    });
+    var cswiper = new Swiper('.column-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+    });
+    var swiper = new Swiper('.page-swiper');
+
+    /*搜索栏*/
     $("#search").click(function(e) {
         $("#form_search").show("slow");
     });
@@ -7,4 +35,24 @@ $(function(){
     $("[data-close]").click(function(e) {
         $(this).parents("#form_search").hide("slow");
     });
+    /*var num=$(".swiper-wrapper").css("transform").replace(/[^0-9\-,]/g,'').split(',')[4];
+    console.log(num);
+    if (num==0){
+        $(".menu-button a").each(function(){
+                $(this)
+                    .addClass("is-actived")
+                    .siblings()
+                    .removeClass("is-actived");
+        });
+    }else{
+        $(this)
+            .removeClass("is-actived");
+    }*/
+
+    /*if($('.swiper-slide').hasClass('swiper-slide-active')) {
+        $(".menu-button a")
+            .addClass("is-actived")
+            .siblings()
+            .removeClass("is-actived");
+    }*/
 });
